f16bd2a4ce3c273bb98c10384b7cf626aa321f8e
Chendeli <419287484@qq.com>
2025-02-18 17:28:21 +0800

    debug

--- src/components/MessageQueue/index.ts
+++ src/components/MessageQueue/index.ts
@@ -1,28 +1,40 @@
-/*
+/**
+ * @format
  * @Author: 陈德立*******419287484@qq.com
  * @Date: 2024-03-28 09:55:15
- * @LastEditTime: 2024-03-28 11:26:01
+ * @LastEditTime: 2025-02-18 17:21:34
+ * @LastEditors: 陈德立*******419287484@qq.com
+ * @Github: https://github.com/Alan1034
+ * @Description:
+ * @FilePath: \network-spanner\src\components\MessageQueue\index.ts
+ */
+
+/**
+ * @format
+ * @Author: 陈德立*******419287484@qq.com
+ * @Date: 2024-03-28 09:55:15
+ * @LastEditTime: 2025-02-18 17:20:34
  * @LastEditors: 陈德立*******419287484@qq.com
  * @Github: https://github.com/Alan1034
  * @Description: 消息队列，用于传入多个数据（如用户多次拖拽）后直接展示最新数据
  * @FilePath: \network-spanner\src\components\MessageQueue\index.ts
- * 
  */
-let getMessageList = <any>[]//消息队列
-let loading = false
+
+let getMessageList = <any>[]; //消息队列
+let loading = false;
 export interface MessageQueueParams {
-  value: any // 队列的内容，会作为传入回调函数的参数
-  somePromise: Function // 队列排队结束时的回调函数，请传入同步函数或者有回调的异步函数
-  delay?: number //当不支持window.requestAnimationFrame时可以自定义setTimeout的延迟时间，默认是1000
+  value?: any; // 队列的内容，会作为传入回调函数的参数
+  somePromise: Function; // 队列排队结束时的回调函数，请传入同步函数或者有回调的异步函数
+  delay?: number; //当不支持window.requestAnimationFrame时可以自定义setTimeout的延迟时间，默认是1000
 }
 export const MessageQueueFunc = (params: MessageQueueParams) => {
-  const { value, somePromise, delay } = params
+  const { value = {}, somePromise, delay } = params;
   const id = getMessageList.length;
   getMessageList.push({
     value,
     id,
   });
-  const requestAnimationFrame = window && window.requestAnimationFrame
+  const requestAnimationFrame = window && window.requestAnimationFrame;
   // loading的时候先保存下来，不更新
   const saveEnterInfo = (i: number) => {
     (requestAnimationFrame || setTimeout)(async () => {
@@ -40,14 +52,12 @@ export const MessageQueueFunc = (params: MessageQueueParams) => {
         const Info = getMessageList.at(-1);
         // console.log("getMessageList", getMessageList)
         getMessageList = [];
-        if (Info?.value) {
-          loading = true
-          await somePromise(Info.value);
-          loading = false
-        }
+        loading = true;
+        await somePromise(Info?.value);
+        loading = false;
       }
       // @ts-ignore
-    }, (delay || 1000));
+    }, delay || 1000);
   };
   saveEnterInfo(id);
-}
+};
